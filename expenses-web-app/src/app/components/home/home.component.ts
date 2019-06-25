0b8b7173178936f3525f79f5f365e6fc3ad7f545
agtourcj3 <lucian.maniu@gmail.com>
2019-06-25 12:33:32 +0300

    Added Angular expenses-webapp first commit

---
+++ expenses-web-app/src/app/components/home/home.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    private router: Router,
+  ) {
+
+    }
+
+  ngOnInit() {
+    // const currentRoute = this.router.url;
+
+    // if (currentRoute === '/users') {
+    //   this.router.navigate(['/users']);
+    // } else {
+    //   this.router.navigate(['/expenses']);
+    // }
+  }
+
+  home() {
+    this.router.navigate(['']);
+  }
+
+  userManagement() {
+    this.router.navigate(['/users']);
+  }
+
+  flowersManagement() {
+    this.router.navigate(['/expenses']);
+  }
+
+  logout() {
+  }
+}
